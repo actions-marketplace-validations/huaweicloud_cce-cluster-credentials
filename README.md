7a41fe25cc3622a72de65e5ccd5c478044c37545
liusheng <liusheng2048@gmail.com>
2021-04-08 11:52:48 +0800

    Add Huawei Cloud cce-cluster-credentials action

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# cce-cluster-credentials
+# Log in to a container registry
+Use this GitHub Action to [get HuaweiCloud CCE cluster credential](https://www.huaweicloud.com/en-us/product/cce.html).
+```yaml
+- uses: huaweicloud/cce-cluster-credentials@v1
+  with:
+    region: '<region id>' # example: ap-southeast-3
+    access-key-id: '<access key id>'
+    access-key-secret: '<access key secret>'
+    project-id: '<project id>'
+    cluster-id: '<CCE cluster id>'
+```
+
+## Reference
+The AK/SK signature algorithm implementation and http request is refer to [hwcce-k8s](https://www.npmjs.com/package/hwcce-k8s).
---
+++ src/index.js
@@ -0,0 +1,30 @@
+const core = require('@actions/core');
+const io = require('@actions/io');
+
+const path = require('path');
+const fs = require('fs');
+
+const http_request = require('./http_request');
+
+
+async function run() {
+    let ak = core.getInput('access-key-id', { required: true });
+    let sk = core.getInput('access-key-secret', { required: true });
+    let region = core.getInput('region', { required: true });
+    let project_id = core.getInput('project-id', { required: true });
+    let cluster_id = core.getInput('cluster-id', { required: true });
+    let url = `https://cce.${region}.myhuaweicloud.com/api/v3/projects/${project_id}/clusters/${cluster_id}/clustercert`;
+    http_request.init_sign({AccessKey:`${ak}`, SecretKey:`${sk}`});
+    config = await http_request.http_request("POST", url, '', {"duration": 1});
+
+    const runnerTempDirectory = process.env['RUNNER_TEMP']; // Using process.env until the core libs are updated
+    const kubeconfigPath = path.join(runnerTempDirectory, `kubeconfig_${Date.now()}`);
+    core.debug(`Writing kubeconfig contents to ${kubeconfigPath}`);
+    fs.writeFileSync(kubeconfigPath, JSON.stringify(config));
+    fs.chmodSync(kubeconfigPath, '600');
+    core.setSecret(kubeconfigPath);
+    core.exportVariable('KUBECONFIG', kubeconfigPath);
+    console.log('KUBECONFIG environment variable is set');
+}
+
+run().catch(e => core.setFailed(e));
